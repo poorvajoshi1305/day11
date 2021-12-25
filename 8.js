60cbc3cf07807662498545f20f99d1b5c08a465f
Santosh Kumar Mondal <SantoshKumar.Mondal@VERIFONE.com>
2021-12-25 12:09:54 +0530

    8

--- 8.js
+++ 8.js
@@ -1,3 +1,33 @@
+function tweet() {
+  let parent = document.querySelector("#parent");
+
+  let prevChilds = parent.innerHTML;
+
+  let tweetVal = document.querySelector("#tweetBox").value;
+
+  let newChild = `<div class="col-12 p-1">
+    <div class="alert alert-success">
+        ${tweetVal}
+
+      <div class="mt-2">
+        <span onclick="like(this)" style="cursor: pointer"
+          >&#128077;</span
+        >
+        <span>0</span>
+
+        <span class="ms-2" onclick="dislike(this)" style="cursor: pointer"
+          >&#128078;</span
+        >
+        <span>0</span>
+      </div>
+    </div>
+  </div>`;
+
+  parent.innerHTML = newChild + prevChilds;
+
+  document.querySelector("#tweetBox").value = "";
+}
+
 function like(param1) {
   let span = param1.parentElement.children[1];
 
